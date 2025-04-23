fc0dcbdd543c9c7615d9621653eddff14742f04d
jvdjo <jv.dd.senai.sc@gmail.com>
2025-04-23 12:12:32 -0300

    Refactor axios client configuration to include Accept header and Access-Control-Allow-Origin

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  },
+  withCredentials: false
 });
 
 export const getWeatherForecast = async () => {
