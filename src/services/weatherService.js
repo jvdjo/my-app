52da06ff8569dce72f4f5940763c66c332a72e48
jvdjo <jv.dd.senai.sc@gmail.com>
2025-05-01 21:20:23 -0300

    Remove custom headers from axios client configuration in weather service

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,11 +2,6 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  },
-  withCredentials: false
 });
 
 export const getWeatherForecast = async () => {
